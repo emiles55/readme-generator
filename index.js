35528481b6b3d8882b23db3e919a6395b53833cc
Emily Miles <emilymiles193@gmail.com>
2023-04-16 22:35:47 -0400

    worked on questions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = import('inquirer');
 const fs = import('fs');
 const generateMarkdown = import("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
+function getQuestions(){
 inquirer.prompt( [{
     type: "input",
     name: "title",
@@ -13,11 +14,27 @@ inquirer.prompt( [{
     name: "description",
     message: "Give a short description of the project: "
 },
+{
+   type:"input",
+   name: "installation",
+   message: "Provide installation information:"
+},
+{
+    type:"input",
+    name: "usage",
+    message: "Provide usage information:"
+ },
+ {
+        type: "list",
+        name: "license",
+        message: "Pick a license for the project: ",
+        choices: ["MIT", "GNU GPL v3", "Unlicense", "None"]
+ },
 ])
 .then((answer) => {
     console.log("Hello " + answer.user_name);
   });
-
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
    fs.writeFile(fileName, generateMarkdown.generateMarkdown(data), (err) =>{
@@ -28,6 +45,7 @@ function writeToFile(fileName, data) {}
    })
 // TODO: Create a function to initialize app
 function init() {}
+getQuestions();
 
 // Function call to initialize app
 init();

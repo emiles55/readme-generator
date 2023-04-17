add83918f0cfac789d55c57d9d514ca5c99d7bd1
Emily Miles <emilymiles193@gmail.com>
2023-04-16 23:17:02 -0400

    worked on generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -13,7 +13,20 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
 
+  ## Table of Contents
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [Testing](#tests)
+
+
+  ## Installation
+${data.installation}
+## Usage
+${data.usage}
 `;
 }
 
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const inquirer = import('inquirer');
 const fs = import('fs');
 const generateMarkdown = import("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-function getQuestions(){
-inquirer.prompt( [{
+
+const questions= [{
     type: "input",
     name: "title",
     message: "What is your project title?"
@@ -22,30 +22,51 @@ inquirer.prompt( [{
 {
     type:"input",
     name: "usage",
-    message: "Provide usage information:"
+    message: "Describe how the project is used:"
  },
  {
         type: "list",
         name: "license",
         message: "Pick a license for the project: ",
-        choices: ["MIT", "GNU GPL v3", "Unlicense", "None"]
+        choices: ["MIT", "GNU", "Unlicensed", "Apache", "Other"]
  },
-])
-.then((answer) => {
-    console.log("Hello " + answer.user_name);
-  });
-}
+ {
+    type: "input",
+    name: "contributing",
+    message: "Who is contributing to the project:"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Please provide testing instructions: "  
+},
+{
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username?"
+},
+{
+    type: 'input',
+    name: 'email',
+    message: "What is your email address?"
+    
+},
+]
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-   fs.writeFile(fileName, generateMarkdown.generateMarkdown(data), (err) =>{
+function writeToFile(fileName, data) {
+   fs.writeFile(fileName, generateMarkdown(data), (err) =>{
     if(err){
         throw new Error(err);
     }
     console.log("File was written successfuly.");
    })
+}
 // TODO: Create a function to initialize app
-function init() {}
-getQuestions();
-
+function init() {
+    inquirer.prompt(questions).then((response) =>
+        writeToFile('README-sample.md', response)
+    )
+}
 // Function call to initialize app
 init();
